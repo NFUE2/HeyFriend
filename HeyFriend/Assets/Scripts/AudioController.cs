be50d1d6bc95f0b7fbf6fe7cf5918f14e8e6a763
Kwaksangwon93 <hs32456a@naver.com>
2024-06-19 12:11:18 +0900

    Add StartScene
    스타트씬 내용 추가

---
+++ HeyFriend/Assets/Scripts/AudioController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetMasterVolume(float volume)
+    {
+        volume = Mathf.Clamp(volume, 0.0001f, 1f);
+        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        volume = Mathf.Clamp(volume, 0.0001f, 1f);
+        audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetEffectVolume(float volume)
+    {
+        volume = Mathf.Clamp(volume, 0.0001f, 1f);
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
+}
