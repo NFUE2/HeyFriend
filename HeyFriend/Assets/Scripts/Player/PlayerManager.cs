8276cd88c548e3467a2ee1b298bf3e902a95fa9f
RaeGyu <wogns9769@gmail.com>
2024-06-19 20:34:19 +0900

    +Add
    플레이어 탑쌓기
    밑에 플레이어의 움직임은 위로 잘 전달이 된다.
    그런데 플레이어 위에서 자유로운 움직임이 되지 않는다.
    IsMove를 사용해서 움직임이 있을때 없을때를 구분해야할 것 같다.

--- HeyFriend/Assets/Scripts/Player/PlayerManager.cs
+++ HeyFriend/Assets/Scripts/Player/PlayerManager.cs
@@ -3,18 +3,37 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    Vector2 position = new Vector2(0,0);
+    //Vector2 position = new Vector2(0,0);
     // Start is called before the first frame update
+    private Rigidbody2D rigid;
+    private PlayerMoveController playerMoveController;
     void Start()
     {
-            PhotonNetwork.Instantiate("Player", position, Quaternion.identity);
-
-        
+        rigid=GetComponent<Rigidbody2D>();
+        playerMoveController = GetComponent<PlayerMoveController>();
+        //PhotonNetwork.Instantiate("Player", position, Quaternion.identity);  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) { //other값이 밟은 사람
+        if(other.gameObject.CompareTag("Player")){
+            other.transform.SetParent(transform);
+
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            other.gameObject.GetComponent<PlayerMoveController>().AddParentVelocity(rigid.velocity.x);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            other.transform.SetParent(null);
+        }
     }
 }
