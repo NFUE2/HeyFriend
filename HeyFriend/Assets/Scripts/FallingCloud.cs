77bc8e35739d543c81032efe354972846059dff5
RaeGyu <wogns9769@gmail.com>
2024-06-20 10:42:30 +0900

    +Add
    떨어지는 구름 애니메이션

--- HeyFriend/Assets/Scripts/FallingCloud.cs
+++ HeyFriend/Assets/Scripts/FallingCloud.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class FallingCloud : MonoBehaviour
 {
     private Rigidbody2D rigid;
     private BoxCollider2D boxCollider2D;
+    private PhotonView pv;
+    private Animator anim;
     // Start is called before the first frame update
     private void Awake() {
         rigid = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
+        pv = GetComponent<PhotonView>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Player")){
+            //pv.RPC("FallingCoroutine",RpcTarget.All);
             StartCoroutine(Falling());
         }
     }
+    public void FallingCoroutine(){
+        StartCoroutine(Falling());
+    }
     IEnumerator Falling(){
+        yield return new WaitForSecondsRealtime(0.3f);
+        anim.SetTrigger("On");
         yield return new WaitForSecondsRealtime(1f);
         rigid.constraints = RigidbodyConstraints2D.None;
         boxCollider2D.isTrigger = true;
