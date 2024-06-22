f0f873d0c7ffb0fb31a0dce033526d57e0e628d9
RaeGyu <wogns9769@gmail.com>
2024-06-22 16:38:40 +0900

    Player안의 오브젝트를 건드리기 전입니다.

--- HeyFriend/Assets/Scripts/Manager/PlayerManager.cs
+++ HeyFriend/Assets/Scripts/Manager/PlayerManager.cs
@@ -6,25 +6,11 @@ public class PlayerManager : MonoBehaviour
 {
     //Vector2 position = new Vector2(0,0);
     // Start is called before the first frame update
-    private Rigidbody2D rigid;
-    private PlayerMoveController playerMoveController;
+
     private PhotonView photonView;
 
-    private bool check;
-    void Start()
-    {
-        rigid=GetComponent<Rigidbody2D>();
-        playerMoveController = GetComponent<PlayerMoveController>();
-        photonView = GetComponent<PhotonView>();
-    }
-    private void OnCollisionEnter2D(Collision2D other) {
-        bool isFoot = other.contacts[0].normal == Vector2.down;
-        if (other.gameObject.CompareTag("Player")&& isFoot)
-        {
-            Debug.Log("비비는 중 ");
-            //other.gameObject.GetComponent<PlayerMoveController>().AddParentVelocity(rigid.velocity.x);
-        }
-    }
+
+
     public void SetPosition(){
         photonView.RPC("SetPositionRPC",RpcTarget.All);
     }
