04de68e6fef14842496daf882d1b0d9c661a20e3
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-06-24 11:17:25 +0900

    Update GameOver.cs

--- HeyFriend/Assets/Scripts/Stage/GameOver.cs
+++ HeyFriend/Assets/Scripts/Stage/GameOver.cs
@@ -1,12 +1,39 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOver : MonoBehaviour
+public class GameOver : MonoBehaviourPunCallbacks, IPunObservable
 {
-    private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.CompareTag("Player")){
-            other.gameObject.GetComponent<PlayerManager>().SetPosition();
+    bool collision = false;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            if (collision)
+            {
+                var players = GameObject.FindGameObjectsWithTag("Player");
+
+                foreach (var p in players)
+                    p.GetComponent<PlayerManager>().SetPosition();
+
+                collision = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log(collision);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            collision = true;
+            //other.gameObject.GetComponent<PlayerManager>().SetPosition();
         }
     }
 }
