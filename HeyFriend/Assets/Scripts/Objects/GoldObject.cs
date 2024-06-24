eb1132e5448cda705a8b931bb9201f7b108f682d
whew15 <huna94@naver.com>
2024-06-24 12:15:15 +0900

    Finish, Coin FIx

--- HeyFriend/Assets/Scripts/Objects/GoldObject.cs
+++ HeyFriend/Assets/Scripts/Objects/GoldObject.cs
@@ -12,8 +12,9 @@ public class GoldObject : MonoBehaviour
     private void Start()
     {
         GameObject objectManager = GameObject.Find("ObjectManager");
+        GameObject audioManager = GameObject.Find("GoldSound");
         stage = objectManager.GetComponent<Stage>();
-        
+        source = audioManager.GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
